11979bce90ab42bde4562b92b36981c70f609add
David Shue <dshue1@babycenter.com>
2014-12-05 09:38:24 -0800

    added speculate

--- src/com/mentat/speculate/SpeculateTest.groovy
+++ src/com/mentat/speculate/SpeculateTest.groovy
@@ -2,9 +2,11 @@ package com.mentat.speculate;
 
 import static groovyx.gpars.GParsPool.speculate
 import static groovyx.gpars.GParsPool.withPool
-import static org.junit.Assert.*;
+import static groovyx.gpars.dataflow.Dataflow.task
+import static org.junit.Assert.*
+import groovyx.gpars.dataflow.DataflowQueue
 
-import org.junit.Test;
+import org.junit.Test
 
 class SpeculateTest {
 
@@ -29,6 +31,39 @@ class SpeculateTest {
 		withPool(4) {
 			println speculate([alternative1, alternative2, alternative3, alternative4]).contains('groovy')
 		}
+	}
+	
+	@Test
+	void testWithDataFlow() {
+		def alternative1 = {
+			'http://www.dzone.com/links/index.html'.toURL().text
+		}
+		
+		def alternative2 = {
+			'http://www.dzone.com/'.toURL().text
+		}
+		
+		def alternative3 = {
+			'http://www.dzzzzzone.com/'.toURL().text  //will fail due to wrong url
+		}
+		
+		def alternative4 = {
+			'http://dzone.com/'.toURL().text
+		}
+		
+		//Pick either one of the following, both will work:
+		final def result = new DataflowQueue()
+		//  final def result = new DataflowVariable()
+		
+		[alternative1, alternative2, alternative3, alternative4].each {code ->
+			task {
+				try {
+					result << code()
+				} catch (ignore) { }  //We deliberately ignore unsuccessful urls
+			}
+		}
+		
+		println result.val.contains('groovy')
 	}
 
 }
