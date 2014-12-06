d1704e10789ba3588c0eb9e64a8c9ea77c5d6923
David Shue <dshue1@babycenter.com>
2014-12-05 18:44:08 -0800

    functions are not suited for async or asyncFun

--- src/com/mentat/async/AsyncTest.groovy
+++ src/com/mentat/async/AsyncTest.groovy
@@ -13,6 +13,7 @@ class AsyncTest {
 		withPool {
 			println slow.callAsync('abc')
 			println fast('xyz')
+			println fun('123')*.callAsync()
 		}
 	}
 	
@@ -22,5 +23,16 @@ class AsyncTest {
 	
 	Closure slow = { a ->
 		println a
+	}
+	
+	/**
+	 * functions are not suited for async, using closure instead.
+	 * You can still call it through async or asyncFun, but you cannot work the Future and Promise
+	 * magic
+	 * @param s
+	 * @return
+	 */
+	protected fun(String s) {
+		slow.call(s)
 	}
 }
