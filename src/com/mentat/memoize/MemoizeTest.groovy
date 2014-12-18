7df15d7f4e402b7af9722933c1bb486fbadb3917
David Shue <dshue1@babycenter.com>
2014-12-18 15:43:25 -0800

    Memoize fun

---
+++ src/com/mentat/memoize/MemoizeTest.groovy
@@ -0,0 +1,29 @@
+package com.mentat.memoize;
+
+import static groovyx.gpars.GParsPool.withPool
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+class MemoizeTest {
+	private static def LOOKUP = [1:'david', 2:'jerry', 3:'justin', 4:'sarah']
+
+	@Test
+	void test() {
+		withPool {
+			Closure nameFetcher = {
+					getName(it)
+			}.gmemoize()
+			
+			println nameFetcher(1)
+			println nameFetcher(1)
+			println nameFetcher(2)
+			println nameFetcher(2)
+		}
+	}
+
+	private String getName(int id) {
+		println 'looking up'
+		LOOKUP[id]
+	}
+}
